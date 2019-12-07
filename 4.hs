e9f34fdeb94785d7f50f2ffe23652d6ced525d83
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2019-12-06 22:45:54 -0500

    better day 4

--- 4.hs
+++ 4.hs
@@ -1,8 +1,38 @@
-import Data.List  (group, sort)
+import Data.Char   (isDigit, ord)
+import Data.List   (group, sort)
+import Data.Maybe  (catMaybes)
 
 main :: IO ()
 main = do
-  [a, b] <- fmap (map read . lines . map (\ ch -> if ch == '-' then '\n' else ch)) getContents
-  let gs = map (sort . map length . group) . filter (\ ds -> ds == sort ds) . map show $ [a :: Integer .. b]
-  print . length . filter ((> 1) . last) $ gs
-  print . length . filter (elem 2) $ gs
+  gs <- fmap ( catMaybes
+             . map (maybeMinimum . filter (>= 2) . map length . group)
+             . filter nondecreasing
+             . map show
+             . uncurry enumFromTo
+             . parse
+             ) getContents
+  print . length $ gs
+  print . length . filter (== 2) $ gs
+
+nondecreasing :: Ord a => [a] -> Bool
+nondecreasing xs = and . zipWith (<=) xs $ tail xs
+
+maybeMinimum :: Ord a => [a] -> Maybe a
+maybeMinimum [] = Nothing
+maybeMinimum xs = Just (minimum xs)
+
+parse :: String -> (Integer, Integer)
+parse = goLeft 0
+  where
+    appendDigit :: Integer -> Char -> Integer
+    appendDigit x d = 10*x + toInteger (ord d - ord '0')
+    
+    goLeft :: Integer -> String -> (Integer, Integer)
+    goLeft m ('-':xs) = goRight m 0 xs
+    goLeft m (d  :xs) = goLeft (appendDigit m d) xs
+    
+    goRight :: Integer -> Integer -> String -> (Integer, Integer)
+    goRight m n ""     = (m, n)
+    goRight m n (d:xs) = if isDigit d then goRight m (appendDigit n d) xs
+                                      else (m, n)
+
